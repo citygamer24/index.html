926ea17da1d365aff7e72c137423ad435f8ef92f
citygamer24 <omaralejandroramirezarias@gmail.com>
2024-11-28 12:03:49 -0600

    funciona el nav

--- indexj.js
+++ indexj.js
@@ -8,5 +8,19 @@ window.onload = function() {
         setTimeout(function() {
             document.getElementById("main-content").classList.add("show");
         }, 1000);  // Tiempo de la animación de la pantalla de bienvenida
-    }, 2000);  // Tiempo que permanece visible la pantalla de bienvenida
+    }, 200);  // Tiempo que permanece visible la pantalla de bienvenida
 };
+// Función para cambiar el contenido según la opción de nav
+function changeContent(section) {
+    // Ocultar todos los contenidos
+    const contents = document.querySelectorAll('.content');
+    contents.forEach(content => content.style.display = 'none');
+  
+    // Mostrar el contenido relacionado con la sección seleccionada
+    document.getElementById(section).style.display = 'block';
+  
+    // Cambiar la clase activa en el navbar
+    const links = document.querySelectorAll('.nav-link');
+    links.forEach(link => link.classList.remove('active'));
+    document.getElementById('nav-' + section).classList.add('active');
+  }
